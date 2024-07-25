c8815cf3ab277641a97b7821b330d29696c0c09e
Abdullah Salman <abdullah.salman@devsinc.com>
2024-07-25 16:05:47 +0500

    user and inventory work done backend|

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const connectDB = require("./db/conn");
 require("dotenv").config();
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 const userRouter = require("./routes/authRoutes");
 const cors = require("cors");
@@ -11,15 +11,16 @@ const cors = require("cors");
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 app.use("/api/v1/auth", userRouter);
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 
-const port = 8000;
+const port = 8001;
 const start = async () => {
   try {
     await connectDB(process.env.CONNSTRING);
-    app.listen(port, console.log("listening on 8000"));
+    app.listen(port, console.log("listening on 8001"));
   } catch (error) {
     console.log(error);
   }
